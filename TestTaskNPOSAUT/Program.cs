0a3f31687825d4faa788d46840a84fcb98899943
Виталий Вишневский <goodaccs@protonmail.com>
2023-03-10 21:46:41 +0500

    update server

--- TestTaskNPOSAUT/Program.cs
+++ TestTaskNPOSAUT/Program.cs
@@ -4,6 +4,8 @@ using System.Text;
 using System.Runtime.Serialization.Json;
 using System.Text.Json;
 using System.Security.Cryptography;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 internal class Program
 {
@@ -39,30 +41,58 @@ internal class Program
             } while (bytes > bufferSize);
             Console.WriteLine(response);
 
-            try {
-                var jsonData = JsonSerializer.Deserialize<List<Dictionary<string, Dictionary<string, string>>>>(response.ToString());
-                foreach (var elem in jsonData)
+            JObject? jsonDict = null;
+            JArray? jsonArray = null;
+            IEnumerable<JToken?> listJsonKeys;
+            try
+            {
+                
+                if (response[0] == '[')
                 {
-                    if (elem.ContainsKey("data"))
-                    {
-                        elem["data"]["processed"] = "true";
-                    }
+                    jsonArray = JArray.Parse(response.ToString());
+                    listJsonKeys = jsonArray.SelectTokens("..data");
+                } else
+                {
+                    jsonDict = JObject.Parse(response.ToString());
+                    listJsonKeys = jsonDict.SelectTokens("..data");
                 }
-                var json = JsonSerializer.Serialize(jsonData);
-                var data = Encoding.UTF8.GetBytes(json);
+
+                foreach (var datapath in listJsonKeys)
+                {
+                    
+                    if (datapath is not null)
+                    {
+                        datapath["processed"] = true;
+                    };
+                }
+
+                var data = Encoding.UTF8.GetBytes(jsonDict is null ? jsonArray.ToString() : jsonDict.ToString());
+
                 await stream.WriteAsync(data);
-            } catch (JsonException jex) {
+
+            }
+            catch (JsonReaderException jsrex)
+            {
+                Console.WriteLine(jsrex);
                 await stream.WriteAsync(Encoding.UTF8.GetBytes(response.ToString()));
-            } 
+            }
+            catch (System.Text.Json.JsonException jex)
+            {
+                Console.WriteLine(jex.Message);
+                await stream.WriteAsync(Encoding.UTF8.GetBytes(response.ToString()));
+            } catch (InvalidOperationException ioe) {
+                Console.WriteLine(ioe.Message);
+                await stream.WriteAsync(Encoding.UTF8.GetBytes(response.ToString()));
+            }
             
             client.Close();
             stream.Close();
         }
     }
-
-    public static async Task TCPSockets() 
+    
+/*    public static async Task TCPSockets() 
     {
-        /*Console.WriteLine("Hello, World!");*/
+        *//*Console.WriteLine("Hello, World!");*//*
         IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 8888);
         Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         socket.Bind(ipPoint);   // связываем с локальной точкой ipPoint
@@ -74,16 +104,16 @@ internal class Program
         NetworkStream stream = new NetworkStream(client);
         int buffersize = 512;
         byte[] responseData = new byte[buffersize];
-        /*var bytes = await stream.ReadAsync(responseData)*/
+        *//*var bytes = await stream.ReadAsync(responseData)*//*
         var response = new StringBuilder();
         int bytes = 1;
-        /*Encoding.UTF8.GetString(responseData, 0, bytes);*/
+        *//*Encoding.UTF8.GetString(responseData, 0, bytes);*//*
         do
         {
             bytes = await stream.ReadAsync(responseData);
             Console.WriteLine(bytes);
             response.Append(Encoding.UTF8.GetString(responseData, 0, bytes));
-            /*Console.WriteLine(bytes);*/
+            *//*Console.WriteLine(bytes);*//*
         } while (bytes > buffersize);
         Console.WriteLine(response);
 
@@ -104,7 +134,6 @@ internal class Program
         client.Close();
 
         
-    }
-
+    }*/
 
 }
